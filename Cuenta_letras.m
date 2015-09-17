0cde8e440b0411d0ab96457362123ea1adff395f
Zombietihu <tonatihu.fi@gmail.com>
2015-09-17 11:09:02 -0500

    Listo

--- Cuenta_letras.m
+++ Cuenta_letras.m
@@ -1,27 +1,17 @@
-Im_RGB=imread('letras.jpg'); 
-J = rgb2gray(Im_RGB);
-
-%imshow(Im_RGB)
-%Filtros
-k = ones(3,3)/9; 
-Im_f = imfilter(J,k);
-g =[1 2 1; 2 4 2; 1 2 1 ]/ 16; %Pasabajas
-Im_g = imfilter(J,g);
-g2 = fspecial('gaussian', [5 5],2);        %Filtro gausiiano con desv. est = 2, empleando funciones propias de matlab
-Im_g2 = imfilter (J, g2);
-%------------------------------------------------------------
-Im_bin=im2bw(Im_g,0.5); %Binarizar imagen
-figure, imshow(Im_bin); %siguiente figura, para que no se sobre escriba
-Im_bin2=not(Im_bin); %Se invierte la imagen
-figure, imshow(Im_bin2) %Se muestra la imagen inversa en otra figura
-SE=strel('square',15); %strel: estructura de elemento, corresponde a un cuadro de 10x10 de puros 1's
-SE2=strel('square',20);
-Im_erode=imerode(Im_bin2,SE); %Se hace la erosion sobre la imagen inversa
-figure, imshow(Im_erode);
-Im_dilate=imdilate(Im_erode,SE2); %Se hace la dilatacion con la misma estructura de elemento
-%figure,imshow(Im_dilate); 
-Iu= not(Im_dilate);
-%figure, imshow(Iu);
-Im_label=bwlabel(Iu,8);
-n=max(max(Im_label));% Se obtiene el numero de etiquetas
+Im_RGB=imread('Letras.jpg'); 
+subplot(2,3,1), imshow(Im_RGB),title('Original')  %Read image and display
+J=rgb2gray(Im_RGB);
+subplot(2,3,2), imshow(J),title('Escala de Grises');%Threshold image and display
+Im_bin=im2bw(J,0.5);
+subplot(2,3,3), imshow(Im_bin),title('Binarizada');%display result
+SE=strel('square',4);
+Im_erode=imerode(Im_bin,SE);
+subplot(2,3,4), imshow(Im_erode),title('Erosión');          %Erode horizontal lines
+boundary=bwperim(Im_erode);
+[i,j]=find(boundary);                 %Superimpose boundaries
+subplot(2,3,5), imshow(Im_RGB),title('Letras ubicadas'); 
+hold on; 
+plot(j,i,'r.');
+Im_label=bwlabel(Im_erode,8);
+n=max(max(Im_label));
 display(n,'El numero de letras que hay es: ');
